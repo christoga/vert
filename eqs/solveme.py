a7a43570de74402954270ad7742e0f1b56d2a496
vickydasta <vickydasta@gmail.com>
2015-11-30 16:58:38 +0700

    ADD: break
    break so there's no unuseful calculation done by script

--- eqs/solveme.py
+++ eqs/solveme.py
@@ -1,22 +1,9 @@
-
-
-# cari solusi persamaan
-
-# dari 3x + 2 = 11
-
+# cari solusi persamaan dari 3x + 2 = 11
 # define hasil = 11
-
 hasil = 11
-
 print "bruteforcing..."
-
 for x in range(1000):
-
     if (3*x) + 2 == hasil:
-
+        break # stop bruteforcing
         print "found!", x
-
-    else:
-
-        print "not found", x
 
