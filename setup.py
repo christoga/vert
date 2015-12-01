2894cb18513d8afb4391b608f960c2f9e52f8dfc
Andre Christoga <andrechristoga@gmail.com>
2015-12-01 13:03:06 +0700

    Update loading message

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setup
 import setuptool
+
 x = input("Insert variable x score ")
 y = input("Insert variable y score ")
-print "Loading..."
-for x in range(1000):
+
+print "Thinking..."
+    for x in range(1000):
     if (3*x) + 2 == y:
         print "Voila! The number is :", x
