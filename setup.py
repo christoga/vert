3f0f983ee7f5b38ac3ed1cb56b5290f8c8e51240
Andre Christoga <andrechristoga@gmail.com>
2015-12-01 09:02:38 +0700

    Remove the code content

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-x = 'y';
-y = 'x';
-
-if x == y:
-    return false;
-else :
-    return true;
-
-if x > y:
-    print x + '=' + y
-break
-
-if x > y:
-    print x + '=' + y
-break
-
-print "Insert the variable"
-raw_input()
+# x = 'y';
+# y = 'x';
+#
+# if x == y:
+#     return false;
+# else :
+#     return true;
+#
+# if x > y:
+#     print x + '=' + y
+# break
+#
+# if x > y:
+#     print x + '=' + y
+# break
+#
+# print "Insert the variable"
+# raw_input()
