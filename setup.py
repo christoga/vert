7ff059ff4bf613ea4d1896378ce1088322c89ada
Andre Christoga <andrechristoga@gmail.com>
2015-12-12 11:40:28 +0700

    Friendly error message

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ score = x * y
 
 if x == y:
 	print "The number have the same value!"
+
 elif x % y == 0:
 	print str(x/y) + "x" +' ' + "= " + str(y/y) + "y" 
 elif y % x == 0:
 	print str(y/x) + "y" +' ' + "= " + str(x/x) + "x" 
+else:
+	print "Something went wrong..."
