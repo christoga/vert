5c77e231ac8ae03c804f59065f6724a068510f6e
Andre Christoga <andrechristoga@gmail.com>
2015-12-12 08:58:40 +0700

    Alert user if variable have the same value

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 import math
 x = input("Insert variable x score ")
 y = input("Insert variable y score ")
+10x = 5y
+
+if "x == y":
+	print "The number have the same value!"
 
 score = x + y
 print score
