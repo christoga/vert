0701f59f0c082a6fd2eeb643ef6b5e4dcee99aea
Andre Christoga <andrechristoga@gmail.com>
2015-12-12 08:43:28 +0700

    Remove action code

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import math
 x = input("Insert variable x score ")
 y = input("Insert variable y score ")
 
-print "Thinking..."
-    for x in range(1000):
-    if (3*x) + 2 == y:
-        print "Voila! The number is :", x
+# print "Thinking..."
+#     for x in range(1000):
+#     if (3*x) + 2 == y:
+#         print "Voila! The number is :", x
