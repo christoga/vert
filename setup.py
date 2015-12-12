f6a3b0ca5d8ff90286252cd2728c7ec099fb4192
Andre Christoga <andrechristoga@gmail.com>
2015-12-12 08:54:07 +0700

    Added score variable

--- setup.py
+++ setup.py
@@ -3,7 +3,4 @@ import math
 x = input("Insert variable x score ")
 y = input("Insert variable y score ")
 
-# print "Thinking..."
-#     for x in range(1000):
-#     if (3*x) + 2 == y:
-#         print "Voila! The number is :", x
+score = x + y
