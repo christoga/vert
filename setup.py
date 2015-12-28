252979c792162ec1a664a00d7c053639a5925475
Andre Christoga <andrechristoga@gmail.com>
2015-12-28 16:29:41 +0700

    Main function

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 import sys
-x = input("Insert variable x score ")
-y = input("Insert variable y score ")
-score = x * y
 
-if x == y:
-	print "The number have the same value!"
+def main():
+	x = input("Insert variable x score ")
+	y = input("Insert variable y score ")
+	score = x * y
 
-elif x % y == 0:
-	print str(x/y) + "x" +' ' + "= " + str(y/y) + "y" 
-    # sys.exit()
-elif y % x == 0:
-	print str(y/x) + "y" +' ' + "= " + str(x/x) + "x" 
-    # sys.exit()
-else:
-	print "Something went wrong..."
+	if x == y:
+		print "The number have the same value!"
+
+	elif x % y == 0:
+		print str(x/y) + "x" +' ' + "= " + str(y/y) + "y" 
+	    # sys.exit()
+	elif y % x == 0:
+		print str(y/x) + "y" +' ' + "= " + str(x/x) + "x" 
+	    # sys.exit()
+	else:
+		print "Something went wrong..."
